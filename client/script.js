c6d5f5ae8f24d80b1c400ab9cf9102a4f50deef8
Sayan Paul <sayanp650@gmail.com>
2023-01-04 20:31:31 +0530

    fourth commit

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   //fetch the data from server -> bot's response
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codex-592d.onrender.com/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
